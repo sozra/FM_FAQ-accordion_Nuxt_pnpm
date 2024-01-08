8d328f1949a44a4d7c4f937d85da797afb19f729
zqm <zeng.qingmao@byd.com>
2024-01-08 18:37:40 +0800

    update

--- app.vue
+++ app.vue
@@ -1,28 +1,86 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
+  <div class="faq-background">
+    <section class="faq-card">
+      <h1>FAQs</h1>
+      <!-- <NuxtWelcome /> -->
+      <article v-for="(item, index) in faqList" :key="index" :id="'article' + index">
+        <!-- <article v-for="(item, index) in faqList" :key="index" :id="'article'+index" :style="{'--max': setMaxHeight(index)}"> -->
+        <button class="accordion-header" :class="{ 'expanded': currentExpandedIndex === index }" :id="'header' + index"
+          @click="test(index)">
+          <!-- <header class="accordion-header expanded" :id="'header' + index"> -->
+          <h2>{{ item.question }}</h2>
+          <img :src="`/_nuxt/public/assets/images/icon-${currentExpandedIndex == index ? 'minus' : 'plus'}.svg`" alt="">
+          <!-- </header> -->
+        </button>
+        <p :id="'answer' + index">{{ item.answer }}</p>
+      </article>
 
-    <article v-for="(item, index) in faqList" :key="index">
-      <header class="accordion-header expanded">
-        <h2>{{ item.question }}</h2>
-        <button></button>
-      </header>
-      <p>{{ item.answer }}</p>
-    </article>
-
+    </section>
   </div>
 </template>
 
 <script setup>
+const currentExpandedIndex = ref(0);
 function test(index) {
   console.log('trigger test function', index);
-  let tag = document.getElementById("testt");
-  const classList = tag.classList;
-  if (classList.contains("test")) {
-    classList.remove("test");
-  } else {
-    classList.add("test");
+  // let tag = document.getElementById(`header${index}`);
+  // const classList = tag.classList;
+  // if (classList.contains("test")) {
+  //   classList.remove("test");
+  // } else {
+  //   classList.add("test");
+  // }
+  const child = document.querySelector(`#answer${index}`);
+  if (!child) return;
+  console.log("child.scrollHeight", child.scrollHeight);
+  console.log(`index: ${index}, currentExpandedIndex: ${currentExpandedIndex.value}`)
+  if (index === currentExpandedIndex.value) {
+    // child.style.maxHeight = child.scrollHeight + 'px';
+    child.style.maxHeight = '0px';
+    currentExpandedIndex.value = -1;
   }
+  else {
+    child.style.maxHeight = child.scrollHeight + 'px';
+    if (currentExpandedIndex.value !== -1) {
+      const prevChild = document.querySelector(`#answer${currentExpandedIndex.value}`);
+      prevChild.style.maxHeight = '0px';
+    }
+    currentExpandedIndex.value = index;
+  }
+  console.log(`at LAST, index: ${index}, currentExpandedIndex: ${currentExpandedIndex.value}`)
+};
+
+onBeforeMount(()=>{
+  console.log('onBeforeMount');
+  const child = document.querySelector(`#answer0`);
+  if (!child) return;
+  child.style.maxHeight = child.scrollHeight + 'px';
+  console.log('init changed')
+})
+
+
+
+
+function setMaxHeight(index) {
+  return Math.floor(Math.random() * 100) + 'px';
+};
+function setMaxHeight1(index) {
+  nextTick().then(() => {
+
+    // const parent = document.querySelector(`article${index}`);
+    const child = document.querySelector(`#answer${index}`);
+    // debugger;
+    console.log('child: ', child);
+
+    if (!child) return;
+    console.log("child.scrollHeight", child.scrollHeight);
+    if (index === currentExpandedIndex.value) {
+      return child.scrollHeight + 'px';
+    }
+    else {
+      return '0px';
+    }
+  })
 };
 
 const faqList = reactive([
@@ -43,7 +101,8 @@ const faqList = reactive([
     way to showcase your skills to potential employers!"
   },
   {
-    question: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    // question: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    question: "How can I get help if I'm stuck on a challenge?",
     answer: "The best place to get help is inside Frontend Mentor's Discord community. There's a help\
     channel where you can ask questions and seek support from other community members."
   }
@@ -53,98 +112,82 @@ const faqList = reactive([
 
 
 <style lang="scss" scoped>
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 1px solid black;
-  padding: 1rem;
+$White: hsl(0, 0%, 100%);
+$Light-pink: hsl(275, 100%, 97%);
+$Grayish-purple: hsl(292, 16%, 49%);
+$Dark-purple: hsl(292, 42%, 14%);
+$font: WorkSans;
 
-
-  // h2 {
-  //   color: red;
-  // }
+:root {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family: $font;
 }
 
-.attribution {
-  a {
-    background-color: red;
-  }
+h1,h2,p,button {
+  font-family: $font;
 }
 
-.accordion-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 1px solid black;
-  padding: 1rem;
 
-  button {
-    // background-color: lightblue;
-    background-color: transparent;
-    border-radius: 30%;
-    border: none;
-    font-size: 2.5rem;
-    transition: all 0.3s ease-in-out;
+div.faq-background {
+  background-image: url('./public/assets/images/background-pattern-desktop.svg');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-color: $Light-pink;
+  width: 100vw;
+  height: 100vh;
 
-    &::before {
-      content: "+";
-    }
+}
 
-    // &:not(.expanded)::before {
-    //   content: "+";
-    // }
-    // &.expanded::before {
-    //   content: "-";
-    // }
-    &:focus,
-    &:active {
-      outline: rgb(from color r g b);
-    }
+section.faq-card {
+  width: 600px;
+  height: 566px;
+  position: absolute;
+  margin: auto auto;
+  padding: 20px;
+  background-color: white;
+  inset: 0;
+  border-radius: 20px;
 
-    &:hover {
-      // background-color: lightblue;
-      scale: 1.2;
-      transform-origin: center;
-    }
-  }
-
-  // button+p {
-  //     max-width: 0;
-  //     overflow: hidden;
-  //     transition: all 0.3s ease-in-out;
-  //     }
-  //   button.expanded+p {
-  //     max-width: 100%;
-  //   }
-
-
-
-  &.expanded {
+  article {
     button {
-      // background-color: lightblue;
-      scale: 1.2;
-      transform-origin: center;
 
-      &::before {
-        content: "-";
+      width: 100%;
+      background-color: white;
+      border: none;
+      display: flex;
+      justify-content: space-between;
+
+
+      h2 {
+        text-align: start;
+        &:hover,
+        &+img:hover,
+        &:has(+img:hover) {
+          color: $Grayish-purple;
+          cursor: pointer;
+        }
       }
+
     }
 
-    &+p {
-      max-height: 100%;
+    p {
+      max-height: 0px;
+      overflow: hidden;
+      transition: max-height .2s ease-in-out;
+      // .expand {
+      //   max-height: var(--max);
+      // }
     }
+
+
+    // &:nth-child(n+3) p {
+    //   max-height: 0px;
+    //   overflow: hidden;
+    //   transition: max-height .2s ease-in-out;
+    // }
+
   }
 }
-
-
-p {
-  max-height: 0;
-  overflow: hidden;
-  transition: all 0.3s ease-in-out;
-}
-
-//   .anwser  {
-//     overflow: hidden;
-//   }
 </style>
